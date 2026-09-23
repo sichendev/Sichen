16a61bf15437c7c6212e7d5a1a776b33d19095cf
Stanislav <sirichenkostas@gmail.com>
2026-09-23 17:06:26 +0300

    - Frontend - Main Page HTML

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express') // Import Express
+const app = express() // Web application
 
-const port = 3000
+const port = 3000 // Port number
 
+// Display the About page on the main route
 app.get('/', (req, res) => {
-  res.send('Hello, fWorld!', "application/json")
+  res.sendFile(__dirname + '/views/about.html')
 })
 
+// Start the web server on the port initialized above
 app.listen(port, () => {
+  // Display the server's address in the console
   console.log(`The server is running on http://localhost:${port}`)
 })
